a1c95ec81c8745944780ad4ab776d53cbe4eaa9e
Richard Honor <14rdh@queensu.ca>
2020-05-07 20:39:39 -0400

    This commit contains all updates up to my second committee meeting

--- GGPlot_Themes.R
+++ GGPlot_Themes.R
@@ -29,6 +29,34 @@ theme_simple<-function(){
     )
 }
 
+theme_simple_vert<-function(){
+  #base theme
+  theme_classic() %+replace%
+    #theme additions
+    theme(
+      
+      #Legend
+      legend.position="top",
+      legend.title = element_blank(),
+      legend.text = element_text(color = "black", size = 14, face = "bold"),
+      legend.background=element_rect(size=0.5,colour="black"),
+      #Theme of axis text and ticks
+      axis.text.x = element_text(color = "black", size = 14, face = "bold"),
+      axis.text.y = element_text(color = "black", size = 14, face = "bold"),
+      axis.ticks = element_blank(),
+      
+      #Theme of Axis titles
+      axis.title.x =  element_text(color = "black", size = 16, face = "bold",margin=margin(10,0,0,0)),
+      axis.title.y =  element_text(color = "black", size = 16,angle = 90, face = "bold",vjust= 0.5,margin=margin(0,10,0,0)),
+      
+      #Plot Margin
+      plot.margin = unit(c(1,1,1,1), "cm"),
+      
+      #Theme of Axis lines
+      axis.line=element_line(colour = "black", size = 1)
+    )
+}
+
 
 theme_simple_multiCol<-function(){
   #base theme
