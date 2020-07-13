b8c03c813ad790d04cefceeff59f3e89d3a5ba3a
Richard Honor <14rdh@queensu.ca>
2020-07-13 16:22:38 -0400

    Random updates

--- GGPlot_Themes.R
+++ GGPlot_Themes.R
@@ -85,6 +85,34 @@ theme_simple_vert<-function(){
     )
 }
 
+theme_simple_vertScientific<-function(){
+  #base theme
+  theme_classic() %+replace%
+    #theme additions
+    theme(
+      
+      #Legend
+      legend.position=c(1.15,0.9),
+      legend.title = element_text(color = "black", size = 16, face = "bold"),
+                                  
+      legend.text = element_text(color = "black", size = 14, face = "bold.italic"),
+      legend.background=element_rect(size=0.5,colour="black"),
+      #Theme of axis text and ticks
+      axis.text.x = element_text(color = "black", size = 14, face = "bold"),
+      axis.text.y = element_text(color = "black", size = 14, face = "bold.italic"),
+      axis.ticks.y = element_blank(),
+      
+      #Theme of Axis titles
+      axis.title.x =  element_text(color = "black", size = 16, face = "bold",margin=margin(10,0,0,0)),
+      axis.title.y =  element_text(color = "black", size = 16,angle = 90, face = "bold",vjust= 0.5,margin=margin(0,10,0,0)),
+                                  
+      #Plot Margin
+      plot.margin = unit(c(1,5,1,1), "cm"),
+      
+      #Theme of Axis lines
+      axis.line=element_line(colour = "black", size = 1)
+    )
+}
 
 theme_simple_multiCol<-function(){
   #base theme
